7f0ab0b5cdf18926362b143467cd059c290800e5
sandekap <129734215+sandekap@users.noreply.github.com>
2025-02-12 10:19:12 -0600

    Initial commit
    Adding the DDL, DML, and the beginning of the HTML files

---
+++ DDL.sql
@@ -0,0 +1,99 @@
+-- CS 340 Project Step 2 Draft
+-- Project Name: Family Planner Database
+-- Team 84
+-- Members: Charles Baker, Kapri Sanders
+
+SET FOREIGN_KEY_CHECKS = 0;
+SET AUTOCOMMIT = 0;
+
+
+-- Creating the tables
+DROP TABLE IF EXISTS Item_Types;
+Create TABLE Item_Types(
+	Type_ID INT NOT NULL AUTO_INCREMENT UNIQUE,
+    Name varchar(255) NOT NULL,
+    PRIMARY KEY (Type_ID)
+);
+
+DROP TABLE IF EXISTS Items_In_House;
+Create TABLE Items_In_House (
+	Item_ID INT NOT NULL AUTO_INCREMENT UNIQUE,
+    Type_ID INT,
+    Name varchar(255) NOT NULL,
+    Quantity INT NOT NULL,
+    Unit varchar(50) NOT NULL,
+    Expiry_Date DATE,
+    PRIMARY KEY (Item_ID),
+    Foreign Key (Type_ID) references Item_Types(Type_ID) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS Shopping_Lists;
+CREATE TABLE Shopping_Lists(
+	Shopping_List_ID INT NOT NULL AUTO_INCREMENT,
+    Name varchar(255),
+    PRIMARY KEY (Shopping_List_ID)
+);
+
+DROP TABLE IF EXISTS Items_Needed;
+CREATE TABLE Items_Needed(
+	Item_ID INT,
+    Shopping_List_ID INT,
+    Quantity INT NOT NULL,
+    PRIMARY KEY(Item_ID, Shopping_List_ID),
+    FOREIGN KEY(Item_ID) References Items_In_House(Item_ID) ON DELETE CASCADE,
+    FOREIGN KEY(Shopping_List_ID) References Shopping_Lists(Shopping_List_ID) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS Recipes;
+CREATE TABLE Recipes(
+	Recipe_ID INT NOT NULL AUTO_INCREMENT UNIQUE,
+    Name varchar(255) NOT NULL,
+    Instructions text NOT NULL,
+    Description text,
+    PRIMARY KEY (Recipe_ID)
+);
+
+DROP TABLE IF EXISTS Ingredients;
+CREATE TABLE Ingredients(
+	Item_ID int,
+    Recipe_ID int,
+    PRIMARY KEY (Item_ID, Table_ID),
+    Foreign Key (Item_ID) References Items_In_House(Item_ID) ON DELETE CASCADE,
+    Foreign Key(Recipe_ID) References Recipes(Recipe_ID) ON DELETE CASCADE
+);
+	
+SET FOREIGN_KEY_CHECKS = 1;
+COMMIT; 
+
+-- Inserting the Data from the example data tables
+INSERT INTO Item_Types(Name)
+VALUES('Cleaning'),
+('Food'),
+('Toiletries');
+
+INSERT INTO Items_In_House(Type_ID, Name, Quantity, Unit, Expiry_Date)
+VALUES(1,'Fabuloso',1,'Bottle', NULL),
+(2,'Bananas',6,'Single', '2025-02-08'),
+(3,'Toilet Paper',1,'Package', NULL),
+(2,'Butter',4,'Sticks', NULL),
+(2,'Flour',1,'kg', NULL);
+
+INSERT INTO Shopping_Lists(Name)
+VALUES('Walmart'),
+('Target'),
+('Sams');
+
+INSERT INTO Items_Needed(Item_ID, Shopping_List_ID, Quantity)
+VALUES(1,1,1),
+(2,1,2),
+(3,1,1);
+
+INSERT INTO Recipes(Name, Instructions, Description)
+VALUES('Banana Bread', 'Bake at 325 for 1 hour', 'Family Recipe'),
+('Tacos','Brown Meat with seasoning and add toppings','Easy Meal'),
+('Mac+Cheese','Cook Pasta and add cheese butter and milk', 'Kid Favorite');
+
+INSERT INTO Ingredients(Item_ID, Recipe_ID)
+VALUES(2,1),
+(4,1),
+(5,1);
---
+++ DML.sql
@@ -0,0 +1,136 @@
+-- Select statements that will support the READ operation for each table
+SELECT * FROM Item_Types
+ORDER BY Type_ID ASC ;
+
+SELECT * FROM Items_In_House
+ORDER BY Name ASC;
+
+SELECT * FROM Recipes
+ORDER BY Recipe_ID ASC;
+
+SELECT * FROM Ingredients
+WHERE Recipe_ID = ?;
+
+SELECT * FROM Shopping_Lists
+ORDER BY Shopping_List_ID ASC;
+
+SELECT * FROM Items_Needed
+WHERE Shopping_List_ID = ?;
+
+-- Insert statements to add items
+
+-- Adds a new item to the house inventory
+INSERT INTO Items_In_House (Type_ID, Name, Quantity, Unit, Expiry_Date)
+VALUES(
+Type_ID = :typeIDInput,
+Name = :nameInput,
+Quantity = :quantityInput,
+Unit = :unitInput
+Expiry_Date = :expiryDateInput
+);
+
+-- Adds a new item type
+INSERT INTO Item_Type (Name)
+Values(Name = :nameInput);
+
+-- Adds a new recipe
+INSERT INTO Recipies( Name, Instructions, Description)
+Values(
+Name = :nameInput,
+Instructions = :instructionsInput,
+Description = :descriptionInput
+);
+
+-- Adds a new ingredient to a recipe
+INSERT INTO Ingredients(Item_ID, Recipe_ID)
+Values(
+Item_ID = :itemIDInput,
+Recipe_ID = :recipeIDInput,
+);
+
+-- Adds a new item to the item needed list
+Insert INTO Items_Needed(Item_ID, Shopping_List_ID, Quantity)
+Values(
+Item_ID = :itemIDInput,
+Shopping_List_ID = : shoppingListIDInput,
+Quantity = :quantityInput
+);
+
+-- Adds a new item to the shopping list
+INSERT INTO Shopping_List(Name)
+Values(Name = :nameInput);
+
+-- Update statements in order to update the items
+
+-- Updates the quantity of an item in the house
+UPDATE Items_In_House
+SET 
+Type_ID = :typeIDInput,
+Name = :nameInput,
+Quantity = :quantityInput,
+Unit = :unitInput,
+Expiry_Date = :expiryDateInput
+WHERE Item_ID = :Item_IDFromUpdateForm;
+
+-- updates the types of items
+UPDATE Item_Types
+SET
+Name = :nameInput
+WHERE Type_ID = : Type_IDFromUpdateForm;
+
+-- updates the recipies
+UPDATE Recipes
+SET
+Name = :nameInput,
+Instructions = :instructionsInput,
+Description = :descriptionInput
+WHERE Recipe_ID = :Recipe_IDFromUpdateForm;
+
+-- updates the ingredients to a recipe
+UPDATE Ingredients
+SET 
+Item_ID = :itemIDInput,
+Recipe_ID = :recipeIDInput,
+WHERE Ingredients_ID = :Ingredients_IDFromUpdateForm;
+
+-- updates the items needed list
+UPDATE Items_Needed
+SET
+Item_ID = :itemIDInput,
+Shopping_List_ID = : shoppingListIDInput,
+Quantity = :quantityInput
+WHERE Items_Needed = : Items_NeededFromUpdateForm;
+
+-- updates the shopping list
+UPDATE Shopping_List
+SET 
+Name = :nameInput
+WHERE Shopping_List_ID = :Shopping_List_IDFromUpdateForm
+
+
+
+-- Delete statements
+
+-- Deletes an item from the house inventory
+DELETE FROM Items_In_House
+WHERE Item_ID = ?;
+
+-- Deletes an item type
+DELETE FROM Item_Types
+WHERE Type_ID = ?;
+
+-- Deletes a recipe
+DELETE FROM Recipes
+WHERE Recipe_ID =?;
+
+-- Deletes an ingredient from a recipe
+DELETE FROM Ingredients
+WHERE Item_ID = ?;
+
+-- Deletes an item from the item needed list
+DELETE FROM Items_Needed
+WHERE Item_ID = ?;
+
+-- Deletes an item from the shopping list
+DELETE FROM Shopping_Lists
+WHERE Item_ID =?;
