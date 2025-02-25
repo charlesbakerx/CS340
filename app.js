902eb0f61de23c48dac4467a4070f0406629d97d
sandekap <129734215+sandekap@users.noreply.github.com>
2025-02-25 13:09:51 -0600

    After step 1
    Here are the file changes after step 1 https://github.com/osu-cs340-ecampus/nodejs-starter-app/tree/main/Step%201%20-%20Connecting%20to%20a%20MySQL%20Database
    I updated the app.js file, and created the db-connector file

--- app.js
+++ app.js
@@ -7,17 +7,44 @@ var express = require('express');   // We are using the express library for the 
 var app     = express();            // We need to instantiate an express object to interact with the server in our code
 PORT        = 5020;                 // Set a port number at the top so it's easy to change in the future
 
+// Database
+var db = require('./database/db-connector')
+
 /*
     ROUTES
 */
-app.get('/', function(req, res)                 // This is the basic syntax for what is called a 'route'
+app.get('/', function(req, res)
     {
-        res.send("The server is running!")      // This function literally sends the string "The server is running!" to the computer
-    });                                         // requesting the web site.
+        // Define our queries
+        query1 = 'DROP TABLE IF EXISTS diagnostic;';
+        query2 = 'CREATE TABLE diagnostic(id INT PRIMARY KEY AUTO_INCREMENT, text VARCHAR(255) NOT NULL);';
+        query3 = 'INSERT INTO diagnostic (text) VALUES ("MySQL is working for sandekaphere!")'; //replace with your onid
+        query4 = 'SELECT * FROM diagnostic;';
 
+        // Execute every query in an asynchronous manner, we want each query to finish before the next one starts
+
+        // DROP TABLE...
+        db.pool.query(query1, function (err, results, fields){
+
+            // CREATE TABLE...
+            db.pool.query(query2, function(err, results, fields){
+
+                // INSERT INTO...
+                db.pool.query(query3, function(err, results, fields){
+
+                    // SELECT *...
+                    db.pool.query(query4, function(err, results, fields){
+
+                        // Send the results to the browser
+                        res.send(JSON.stringify(results));
+                    });
+                });
+            });
+        });
+    });
 /*
     LISTENER
 */
 app.listen(PORT, function(){            // This is the basic syntax for what is called the 'listener' which receives incoming requests on the specified PORT.
-    console.log('Express started on http://localhost:' + PORT + '; press Ctrl-C to terminate.')
+    console.log('Express started on http://classwork.engr.oregonstate.edu:' + PORT + '; press Ctrl-C to terminate.')
 });
