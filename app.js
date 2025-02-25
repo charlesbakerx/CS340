3781de371d3fcdba89ceb9f07fef8744ce55a80e
sandekap <129734215+sandekap@users.noreply.github.com>
2025-02-25 16:52:00 -0600

    step 5
    updated the two files to allow an add, I am getting the following error when adding an item
    'Cannot add or update a child row: a foreign key constraint fails (`cs340_sandekap`.`Items_In_House`, CONSTRAINT `Items_In_House_ibfk_1` FOREIGN KEY (`Type_ID`) REFERENCES `Item_Types` (`Type_ID`) ON DELETE CASCADE)',
      sqlState: '23000',
      index: 0,
      sql: "INSERT INTO Items_In_House (Item_ID, Type_ID, Name, Quantity, Unit, Expiry_Date) VALUES ('7', '1','Bleach','1','Bottle' ,NULL)"

--- app.js
+++ app.js
@@ -16,6 +16,11 @@ var exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
+// Configuring Express to handle json and form data
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(express.static('public'))
+
 
 /*
     ROUTES
@@ -29,6 +34,38 @@ app.get('/', function(req, res)
             res.render('index', {data: rows});                  // Render the index.hbs file, and also send the renderer
         })                                                      // an object where 'data' is equal to the 'rows' we
     });                                                         // received back from the query
+
+    app.post('/add-item-form', function(req, res){
+        // Capture the incoming data and parse it back to a JS object
+        let data = req.body;
+    
+        // Capture NULL values
+        let Expiry_Date = parseInt(data['input-Expiry_Date']);
+        if (isNaN(Expiry_Date))
+        {
+            Expiry_Date = 'NULL'
+        }
+    
+        // Create the query and run it on the database
+        query1 = `INSERT INTO Items_In_House (Item_ID, Type_ID, Name, Quantity, Unit, Expiry_Date) VALUES ('${data['input-Item_ID']}', '${data['input-Type_ID']}','${data['input-Name']}','${data['input-Quantity']}','${data['input-Unit']}' ,${Expiry_Date})`;
+        db.pool.query(query1, function(error, rows, fields){
+    
+            // Check to see if there was an error
+            if (error) {
+    
+                // Log the error to the terminal so we know what went wrong, and send the visitor an HTTP response 400 indicating it was a bad request.
+                console.log(error)
+                res.sendStatus(400);
+            }
+    
+            // If there was no error, we redirect back to our root route, which automatically runs the SELECT * FROM bsg_people and
+            // presents it on the screen
+            else
+            {
+                res.redirect('/');
+            }
+        })
+    })
 /*
     LISTENER
 */
