a02923b04d19ad66db383f8d429ecdc4f888b1fd
Charles Baker <126838351+charlesbakerx@users.noreply.github.com>
2025-03-09 15:40:36 -0500

    Fixes some declarations

--- nodejs/app.js
+++ nodejs/app.js
@@ -3,16 +3,16 @@
 /*
     SETUP
 */
-var express = require('express');   // We are using the express library for the web server
-var app     = express();            // We need to instantiate an express object to interact with the server in our code
-PORT        = 5020;                 // Set a port number at the top so it's easy to change in the future
+let express = require('express');   // We are using the express library for the web server
+let app = express();            // We need to instantiate an express object to interact with the server in our code
+const PORT        = 5020;                 // Set a port number at the top so it's easy to change in the future
 
 // Database
-var db = require('./database/db-connector')
+let db = require('./database/db-connector')
 
 // Use Handlebars
 const { engine } = require('express-handlebars');
-var exphbs = require('express-handlebars');     // Import express-handlebars
+let exphbs = require('express-handlebars');     // Import express-handlebars
 app.engine('.hbs', engine({extname: ".hbs"}));  // Create an instance of the handlebars engine to process templates
 app.set('view engine', '.hbs');                 // Tell express to use the handlebars engine whenever it encounters a *.hbs file.
 
