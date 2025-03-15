962c2485a70246274402e8e1164464e62c1ea723
Charles Baker <126838351+charlesbakerx@users.noreply.github.com>
2025-03-15 18:15:24 -0500

    Changed to using dotenv to load secrets

--- nodejs/database/db-connector.js
+++ nodejs/database/db-connector.js
@@ -1,15 +1,20 @@
 // ./database/db-connector.js
-
+require('dotenv').config()
+const database_url = process.env.DATABASE_URL;
+const database_name = process.env.DATABASE_NAME;
+const database_user = process.env.DATABASE_USER;
+const database_password = process.env.DATABASE_PASSWORD;
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
 
 // Create a 'connection pool' using the provided credentials
+
 var pool = mysql.createPool({
     connectionLimit : 10,
-    host            : 'classmysql.engr.oregonstate.edu',
-    user            : 'cs340_sandekap',
-    password        : '3967',
-    database        : 'cs340_sandekap'
+    host            : database_url,
+    user            : database_user,
+    password        : database_password,
+    database        : database_name
 })
 
 // Export it for use in our applicaiton
