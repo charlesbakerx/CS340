cc696e0cf767131a3ba1bc33a1c511f651b579d0
Charles Baker <126838351+charlesbakerx@users.noreply.github.com>
2025-03-15 18:20:56 -0500

    Sets forgotten path parameter

--- nodejs/database/db-connector.js
+++ nodejs/database/db-connector.js
@@ -1,11 +1,12 @@
 // ./database/db-connector.js
-require('dotenv').config()
+require('dotenv').config({path: __dirname + '/config.env'})
 const database_url = process.env.DATABASE_URL;
 const database_name = process.env.DATABASE_NAME;
 const database_user = process.env.DATABASE_USER;
 const database_password = process.env.DATABASE_PASSWORD;
 // Get an instance of mysql we can use in the app
 var mysql = require('mysql')
+const {configDotenv} = require("dotenv");
 
 // Create a 'connection pool' using the provided credentials
 
