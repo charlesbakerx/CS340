63b56730b2d05136087ad9bda043c8a8e148426c
Charles Baker <126838351+charlesbakerx@users.noreply.github.com>
2025-03-17 16:47:49 -0500

    Updated to match queries actually used in our code.

--- nodejs/database/DML.sql
+++ nodejs/database/DML.sql
@@ -1,151 +1,98 @@
--- Select statements that will support the READ operation for each table
--- Improvement: SELECT queries will need to be updated to utilize a JOIN
---              to provide relevant names to IDs. ;Quinn Downey
-SELECT * FROM Item_Types
-ORDER BY Type_ID ASC ;
+-- Selects --
+SELECT * FROM Item_Types;
+SELECT * FROM Items_In_House;
+SELECT * FROM Item_Types;
+SELECT * FROM Shopping_Lists;
+SELECT * FROM Recipes;
 
-SELECT * 
+SELECT * FROM Item_Types WHERE Type_ID = ':inputID';
+SELECT * FROM Shopping_Lists WHERE Shopping_List_ID = ':inputID';
+SELECT * FROM Recipes WHERE Recipe_ID = ':inputID';
+
+SELECT * FROM Items_In_House WHERE Name LIKE ':inputName%';
+
+-- Used to pass all needed data to the table at / --
+SELECT Items_In_House.Type_ID, Items_In_House.Item_ID,
+       Item_Types.Name AS Type, Items_In_House.Name, Items_In_House.Quantity,
+       Items_In_House.Unit, Items_In_House.Expiry_Date
 FROM Items_In_House
-INNER JOIN Item_Types
-ON Items_In_House.Type_ID = Item_Types.Type_ID 
-ORDER BY Name ASC;
+         LEFT JOIN Item_Types ON Items_In_House.Type_ID = Item_Types.Type_ID;
 
-SELECT * FROM Recipes
-ORDER BY Recipe_ID ASC;
-
-SELECT * 
-FROM Ingredients
-INNER JOIN Recipes
-ON Ingredients.Recipe_ID = Recipes.Recipe_ID
-INNER JOIN Items_In_House
-ON Ingredients.Item_ID = Items_In_House.Item_ID
-WHERE Recipe_ID = ?;
-
-SELECT * FROM Shopping_Lists
-ORDER BY Shopping_List_ID ASC;
-
-SELECT * 
+-- Used to pass all needed data to the table at /items_needed --
+SELECT Items_In_House.Item_ID, Items_In_House.Name, Shopping_Lists.Shopping_List_ID,
+       Shopping_Lists.Name AS Shopping_List, Items_Needed.Quantity
 FROM Items_Needed
-INNER JOIN Items_In_House
-ON Items_Needed.Item_ID = Items_In_House.Item_ID
-INNER JOIN Shopping_Lists
-ON Items_Needed.Shopping_List_ID = Items_Needed.Shopping_List_ID
-WHERE Shopping_List_ID = ?;
+         INNER JOIN Items_In_House ON Items_Needed.Item_ID = Items_In_House.Item_ID
+         INNER JOIN Shopping_Lists ON Items_Needed.Shopping_List_ID = Shopping_Lists.Shopping_List_ID;
 
--- Insert statements to add items
+-- Used to pass all needed data to the table at /ingredients --
+SELECT Ingredients.Item_ID, Ingredients.Recipe_ID, Recipes.Name AS Recipe,
+       Items_In_House.Name AS Item
+FROM Ingredients
+         INNER JOIN Recipes ON Ingredients.Recipe_ID = Recipes.Recipe_ID
+         INNER JOIN Items_In_House ON Ingredients.Item_ID = Items_In_House.Item_ID;
 
--- Adds a new item to the house inventory
-INSERT INTO Items_In_House (Type_ID, Name, Quantity, Unit, Expiry_Date)
-VALUES(
-Type_ID = :typeIDInput,
-Name = :nameInput,
-Quantity = :quantityInput,
-Unit = :unitInput
-Expiry_Date = :expiryDateInput
-);
+-- Used as part of an insert function to check if the item exists before adding it --
+SELECT Item_ID FROM Items_In_House
+WHERE UPPER(Name) LIKE UPPER(':inputName');
 
--- Adds a new item type
-INSERT INTO Item_Type (Name)
-Values(Name = :nameInput);
+-- Inserts --
 
--- Adds a new recipe
-INSERT INTO Recipies( Name, Instructions, Description)
-Values(
-Name = :nameInput,
-Instructions = :instructionsInput,
-Description = :descriptionInput
-);
+-- This version is used if passed a valid date otherwise NULL is passed for Expiry_Date --
+INSERT INTO Items_In_House (Item_ID, Type_ID, Name, Quantity, Unit, Expiry_Date)
+VALUES (':inputItemID', ':inputTypeID', ':inputName',
+        ':inputQuantity', ':inputUnit', ':inputExpiryDate');
 
--- Adds a new ingredient to a recipe
-INSERT INTO Ingredients(Item_ID, Recipe_ID)
-Values(
-Item_ID = :itemIDInput,
-Recipe_ID = :recipeIDInput,
-);
+INSERT INTO Item_Types (Name) VALUES (':inputName');
+INSERT INTO Shopping_Lists (Name) VALUES (':inputName');
+INSERT INTO Items_In_House (Name, Quantity, Unit) VALUES (':inputName', 0, ' ');
 
--- Adds a new item to the item needed list
-Insert INTO Items_Needed(Item_ID, Shopping_List_ID, Quantity)
-Values(
-Item_ID = :itemIDInput,
-Shopping_List_ID = :shoppingListIDInput,
-Quantity = :quantityInput
-);
+INSERT INTO Items_Needed (Item_ID, Shopping_List_ID, Quantity)
+VALUES (':itemID', ':inputListID', ':inputQuantity');
 
--- Adds a new item to the shopping list
-INSERT INTO Shopping_Lists(Name)
-Values(Name = :nameInput);
+INSERT INTO Recipes (Name, Instructions, Description)
+VALUES (':inputName', ':inputInstructions', ':inputDescription');
 
--- Update statements in order to update the items
+INSERT INTO Ingredients (Item_ID, Recipe_ID) VALUES (':inputItemID', ':inputRecipeID');
 
--- Updates the quantity of an item in the house
-UPDATE Items_In_House
-SET
-Type_ID = :typeIDInput,
-Name = :nameInput,
-Quantity = :quantityInput,
-Unit = :unitInput,
-Expiry_Date = :expiryDateInput
-WHERE Item_ID = :Item_IDFromUpdateForm;
+-- Deletes --
 
--- updates the types of items
+DELETE FROM Items_In_House WHERE Item_ID = ':inputID';
+DELETE FROM Item_Types WHERE Type_ID = ':inputID';
+DELETE FROM Shopping_Lists WHERE Shopping_List_ID = ':inputID';
+DELETE FROM Items_Needed WHERE Item_ID = ':inputItemID' AND Shopping_List_ID = ':inputListID';
+DELETE FROM Recipes WHERE Recipe_ID = ':inputID';
+DELETE FROM Ingredients WHERE Item_ID = ':inputItemID' AND Recipe_ID = ':inputRecipeID';
+
+-- Updates --
+
+UPDATE Items_In_House SET
+                          Type_ID     = ':inputTypeID',
+                          Quantity    = ':inputQuantity',
+                          Unit        = ':inputUnit',
+                          Expiry_Date = ':inputExpiryDate'
+WHERE Item_ID = ':inputID';
+
 UPDATE Item_Types
-SET
-Name = :nameInput
-WHERE Type_ID = :Type_IDFromUpdateForm;
+SET Name = ':inputName'
+WHERE Type_ID = ':inputID';
 
--- updates the recipies
-UPDATE Recipes
-SET
-Name = :nameInput,
-Instructions = :instructionsInput,
-Description = :descriptionInput
-WHERE Recipe_ID = :Recipe_IDFromUpdateForm;
-
--- updates the ingredients to a recipe
-UPDATE Ingredients
-SET
-Item_ID = :itemIDInput,
-Recipe_ID = :recipeIDInput,
-WHERE Ingredients_ID = :Ingredients_IDFromUpdateForm;
-
--- updates the items needed list
-UPDATE Items_Needed
-SET
-Item_ID = :itemIDInput,
-Shopping_List_ID = :shoppingListIDInput,
-Quantity = :quantityInput
-WHERE Items_Needed = :Items_NeededFromUpdateForm;
-
--- updates the shopping list
 UPDATE Shopping_Lists
-SET
-Name = :nameInput
-WHERE Shopping_List_ID = :Shopping_List_IDFromUpdateForm
+SET Name = ':inputName'
+WHERE Shopping_List_ID = ':inputID';
+
+UPDATE Items_Needed
+SET Quantity = ':inputQuantity'
+WHERE Item_ID = ':inputItemID' AND Shopping_List_ID = ':inputListID';
+
+UPDATE Recipes
+SET Name         = ':inputName',
+    Instructions = ':inputInstructions',
+    Description  = ':inputDescription'
+WHERE Recipe_ID = ':inputID';
+
+UPDATE Ingredients
+SET Item_ID   = ':inputItemID'
+WHERE Item_ID = ':oldItemID' AND Recipe_ID = ':inputRecipeID';
 
 
-
--- Delete statements
-
--- Deletes an item from the house inventory
-DELETE FROM Items_In_House
-WHERE Item_ID = ?;
-
--- Deletes an item type
-DELETE FROM Item_Types
-WHERE Type_ID = ?;
-
--- Deletes a recipe
-DELETE FROM Recipes
-WHERE Recipe_ID =?;
-
--- Deletes an ingredient from a recipe
-DELETE FROM Ingredients
-WHERE Item_ID = :itemIDInput AND Recipe_ID = recipeIDInput;
-
--- Deletes an item from the item needed list
-DELETE FROM Items_Needed
-WHERE Item_ID = :itemIDInput AND Shopping_List_ID = :shoppingListIDInput;
-
--- Deletes a list from the shopping lists
-DELETE FROM Shopping_Lists
-WHERE Shopping_List_ID = ?;
